565241ca24717b99f01c97ac1823ace35020b70e
ShiHaojie <91363385+mlabalabala@users.noreply.github.com>
2022-12-24 10:47:46 +0800

    Update Gen.py

--- Gen.py
+++ Gen.py
@@ -9,9 +9,8 @@ headers = {
     "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
 }
 
-proxies = {"http": "http://127.0.0.1:10809","https": "https://127.0.0.1:10809",}
-
-# proxies = {"http": None,"https": None,}
+# proxies = {"http": "http://127.0.0.1:10809","https": "https://127.0.0.1:10809",}
+proxies = {"http": None,"https": None,}
 
 
 def modIP(nodes):
