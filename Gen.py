fe267d283af243082075fffadd2b6bbc446c2fbe
mlabalabala <labalabala@protonmail.com>
2023-03-06 15:44:02 +0800

    .

--- Gen.py
+++ Gen.py
@@ -6,13 +6,25 @@ from urllib.parse import unquote
 
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = "TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-256-GCM-SHA384:ECDHE:!COMPLEMENTOFDEFAULT"
 
-headers = {
+header = {
     "User-Agent":
     "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
 }
 
 # proxies = {"http": "http://127.0.0.1:10809","https": "https://127.0.0.1:10809",}
 proxies = {"http": None,"https": None,}
+
+
+def decode_base64(data):
+    """Decode base64, padding being optional.
+    :param data: Base64 data as an ASCII byte string
+    :returns: The decoded byte string.
+
+    """
+    missing_padding = 4 - len(data) % 4
+    if missing_padding:
+        data += '='* missing_padding
+    return str(base64.b64decode(data).decode('UTF-8'))
 
 
 def modIP(nodes):
@@ -22,14 +34,14 @@ def modIP(nodes):
         if (node):
             pro_type,node_det = node.split('://')
             if 'vmess' == pro_type:
-                node_det_json = base64.b64decode(node_det).decode('UTF-8')
+                node_det_json = decode_base64(node_det)
                 node_det_dict = json.loads(node_det_json)
 #                 node_det_dict['ps'] = 'icook.hk-%02.d' % nodes.index(node)
 #                 node_det_dict['ps'] = '%02.d' % nodes.index(node)
                 node_det_dict['ps'] = f'{y}/{m}/{d}-' + node_det_dict['ps']
-#                 node_det_dict['add'] = 'guolicheng.cfd'
+                node_det_dict['add'] = 'guolicheng.cfd'
 #                 node_det_dict['add'] = '104.16.245.116'
-                node_det_dict['add'] = '172.66.43.109' 
+                # node_det_dict['add'] = '172.66.43.109' 
                 node_det_json = json.dumps(node_det_dict)
                 node_mod = 'vmess://'+base64.b64encode(node_det_json.encode()).decode('UTF-8')
                 nodes_mod_list.append(node_mod)
@@ -46,7 +58,7 @@ def GenNodesFile(urls, flag):
 
     for url in urls:
         print(url)
-        res = s.get(url, headers=headers, proxies=proxies)
+        res = s.get(url, headers=header, proxies=proxies)
         text = res.text
         d = '%d' % (int(d) - urls.index(url))
         if "<html" not in text:
@@ -56,7 +68,7 @@ def GenNodesFile(urls, flag):
         nodes = re.findall(r'url=(\S+)&insert=', unquote(text))[0].split('|')
         nodes_ori = base64.b64encode('\n'.join(nodes).encode(encoding='UTF-8')).decode('UTF-8')
     else:
-        nodes = base64.b64decode(text).decode('UTF-8').split('\n')
+        nodes = decode_base64(text).split('\n')
         nodes_ori = text
 
     nodes_b64_en = base64.b64encode(modIP(nodes).encode(encoding='UTF-8'))
@@ -71,21 +83,15 @@ def GenNodesFile(urls, flag):
 def GenNodes4ClashFile(urls, flag):
     for url in urls:
         print(url)
-        res = s.get(url, headers=headers, proxies=proxies)
+        res = s.get(url, headers=header, proxies=proxies)
         text = res.text
         with open(f'{flag}.txt', 'w', encoding='utf8') as w:
             w.write(text)
         if "<html" not in text:
             break
-            
 
-if __name__ == "__main__":
 
-    s = requests.session()
-
-    y = time.strftime('%Y')
-    m = time.strftime('%m')
-    d = time.strftime('%d')
+def GenNods():
 
     clashnode_url = 'https://clashnode.com/wp-content/uploads/{}/{}/{}.txt'
     clashnode_urls = [clashnode_url.format(y, m, y+m+'%02d'%i) for i in range(int(d), 0, -1)]
@@ -93,7 +99,7 @@ if __name__ == "__main__":
     nodefree_url = 'https://nodefree.org/dy/{}/{}/{}.txt'
     nodefree_urls = [nodefree_url.format(y, m, y+m+'%02d'%i) for i in range(int(d), 0, -1)]
     
-    other_urls = ['https://raw.iqiq.io/ermaozi/get_subscribe/main/subscribe/v2ray.txt']
+    # other_urls = ['https://raw.iqiq.io/ermaozi/get_subscribe/main/subscribe/v2ray.txt']
 
     GenNodesFile(clashnode_urls, 'clashnode')
     GenNodesFile(nodefree_urls, 'nodefree')
@@ -108,3 +114,12 @@ if __name__ == "__main__":
     GenNodes4ClashFile(clashnode4clash_urls, 'clashnode4clash')
     GenNodes4ClashFile(nodefree4clash_urls, 'nodefree4clash')
 
+
+if __name__ == "__main__":
+    s = requests.session()
+
+    y = time.strftime('%Y')
+    m = time.strftime('%m')
+    d = time.strftime('%d')
+
+    GenNods()
