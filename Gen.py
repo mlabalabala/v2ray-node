41af7eab6b9f91567f96b2f7ca6ae40ce1383ec7
1842047134@qq.com <91363385+mlabalabala@users.noreply.github.com>
2023-10-31 23:39:06 +0800

    Update Gen.py

--- Gen.py
+++ Gen.py
@@ -109,8 +109,8 @@ def GenNods():
     GenNodesFile(nodefree_urls, 'nodefree')
 #     GenNodesFile(other_urls, 'other')
 # https://sub.id9.cc/sub?target=clash&url=https://clashnode.com/wp-content/uploads/2023/02/20230217.txt&insert=false&config=https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_NoAuto.ini
-    # clashnode4clash_url = 'https://sub.id9.cc/sub?target=clash&url=https://clashnode.com/wp-content/uploads/{}/{}/{}.txt&insert=false&config=https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_NoAuto.ini'
-    clashnode4clash_url = 'https://clashnode.com/wp-content/uploads/{}/{}/{}.yaml'
+    clashnode4clash_url = 'https://sub.id9.cc/sub?target=clash&url=https://clashnode.com/wp-content/uploads/{}/{}/{}.txt&insert=false&config=https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_NoAuto.ini'
+    # clashnode4clash_url = 'https://clashnode.com/wp-content/uploads/{}/{}/{}.yaml'
     clashnode4clash_urls = [clashnode4clash_url.format(y, m, y+m+'%02d'%i) for i in range(int(d), 0, -1)]
 
     # nodefree4clash_url = 'https://sub.id9.cc/sub?target=clash&url=https://nodefree.org/dy/{}/{}/{}.txt&insert=false&config=https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_NoAuto.ini'
