195af989532e3a17a7809562ec40f8baf2e14e17
1842047134@qq.com <91363385+mlabalabala@users.noreply.github.com>
2023-10-31 23:42:09 +0800

    Update Gen.py

--- Gen.py
+++ Gen.py
@@ -5,6 +5,7 @@ from urllib.parse import unquote
 
 
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = "TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-256-GCM-SHA384:ECDHE:!COMPLEMENTOFDEFAULT"
+requests.packages.urllib3.disable_warnings()
 
 header = {
     "User-Agent":
